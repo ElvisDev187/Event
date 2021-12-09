407bd725dfb6854c84356f0a9f568af4c002aa1a
ElvisDev187 <84122281+ElvisDev187@users.noreply.github.com>
2021-12-09 18:23:26 +0100

    Add fonctionality of Sign In an  Sign Up by Gatslage And ElvisDevs

--- app/src/main/java/fr/groupe/event/fragments/LoginFragment.kt
+++ app/src/main/java/fr/groupe/event/fragments/LoginFragment.kt
@@ -1,6 +1,8 @@
 package fr.groupe.event.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.TextureView
@@ -9,30 +11,63 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import fr.groupe.event.MainActivity
 import fr.groupe.event.R
 import org.w3c.dom.Text
+import java.util.regex.Pattern
 
 
 class LoginFragment : Fragment() {
 
-
+    private lateinit  var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        auth = Firebase.auth
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_login, container, false)
            val email  = view.findViewById<EditText>(R.id.edit_text_mail)
         val pwd = view.findViewById<EditText>(R.id.edit_text_pwd)
         val btnLogin = view.findViewById<Button>(R.id.btn_login)
 
-
+            val emaiText = email.text.toString()
 
         btnLogin.setOnClickListener {
-            //fireBase Here
+            if (valideEmail(email) && !(pwd.text.toString().isEmpty())){
+                auth.signInWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = auth.currentUser
+                            if (user?.isEmailVerified()== true){
+                                Toast.makeText(requireContext(), "Authentication Successfully.",
+                                    Toast.LENGTH_SHORT).show()
+                                findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+
+                            }else{
+                                Toast.makeText(requireContext(), "Allez Valider votre adresse mail dans votre boite mail",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(requireContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }else{
+                Toast.makeText(requireContext(), " Mot de passe invalide",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
 
          val linkToRegister = view.findViewById<TextView>(R.id.link)
@@ -42,6 +77,16 @@ class LoginFragment : Fragment() {
 
 
         return view
+    }
+
+    fun valideEmail(edText : EditText) : Boolean{
+        val email = edText.text.toString()
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return  true
+        }else{
+           Toast.makeText(requireContext(),"Adresse mail invalide", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 
 }
--- app/src/main/java/fr/groupe/event/fragments/RegisterEditFragment.kt
+++ app/src/main/java/fr/groupe/event/fragments/RegisterEditFragment.kt
@@ -1,25 +1,31 @@
 package fr.groupe.event.fragments
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import fr.groupe.event.R
 
 class RegisterEditFragment : Fragment() {
-
+    private lateinit  var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_register_edit, container, false)
+        auth = Firebase.auth
 
         val Username = view.findViewById<EditText>(R.id.edit_text_username)
         val UserEmail = view.findViewById<EditText>(R.id.edit_text_mail)
@@ -29,14 +35,64 @@ class RegisterEditFragment : Fragment() {
         val btn = view.findViewById<Button>(R.id.next)
         btn.setOnClickListener{
 
+        if (Username.text.trim().toString().isEmpty()){
+            Toast.makeText(requireContext(), " Veuillez remplir le nom d'utilisateur",
+                Toast.LENGTH_SHORT).show()
+        }else if (!valideEmail(UserEmail)){
 
-            //fireBase Here
+        }else if (!(pwd.text.toString().isEmpty()) && !(Confpwd.text.toString().isEmpty())){
+            if (pwd.text.toString() == Confpwd.text.toString()){
+                auth.createUserWithEmailAndPassword(UserEmail.text.toString(), pwd.text.toString())
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(requireContext(), "C'est Firebase",
+                                Toast.LENGTH_SHORT).show()
+                            val user = auth.currentUser
+                                user?.sendEmailVerification()?.addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+
+                                        Toast.makeText(requireContext(), "Allez Valider votre adresse mail dans votre boite mail",
+                                            Toast.LENGTH_SHORT).show()
+                                        findNavController().
+                                        navigate(R.id.action_viewPagerFragment2_to_loginFragment)
+                                    }
+                                }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(requireContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }else{
+                Toast.makeText(requireContext(), " Les mots de passe ne correspond",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(requireContext(), " Les mots de passe doivent etre remplis",
+                Toast.LENGTH_SHORT).show()
+        }
 
 
 
-            findNavController().navigate(R.id.action_viewPagerFragment2_to_loginFragment)
+
         }
         return view
     }
+
+    fun valideEmail(edText : EditText) : Boolean{
+        val email = edText.text.toString()
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return  true
+        }else{
+            Toast.makeText(requireContext(),"Adresse mail invalide", Toast.LENGTH_SHORT).show()
+            return false
+        }
+    }
+
+
 
 }
